f5096a4585711c175892e4f878753abddcecbe57
Abhishek <abhishek.dahad@gmail.com>
2025-06-05 14:50:41 -0400

    Added eval_epoch() function to training.py to get the results from the epoch. Added Result class to store results from evaluating epochs.

--- training.py
+++ training.py
@@ -1,6 +1,23 @@
 import torch
 import numpy as np
 from model import Conv_Net
+from sklearn import metrics
+from torch.nn.functional import softmax
+
+
+
+class Result:
+
+    def __init__(self,mode : str, epoch : int, accuracy : float, precision : float, f1score : float, recall : float, loss : float):
+        self.mode = mode
+        self.epoch = epoch
+        self.accuracy = accuracy
+        self.precision = precision
+        self.f1score = f1score
+        self.recall = recall
+        self.loss = loss
+
+
 
 def train_epoch(data_loader, model, criterion, optimizer):
     for i, (X, y) in enumerate(data_loader):
@@ -30,7 +47,43 @@ def predict_class(logits):
     """
     return np.argmax(logits, axis = 1)
 
-def eval_epoch():
+def eval_epoch(model, training_loader, validation_loader, testing_loader, criterion, epoch,includes_testing = False):
+
+    def get_stats(loader):
+        y_true, y_pred = [], []
+        running_loss = []
+
+        for i, (X, y) in enumerate(loader):
+            """ 
+            Context manager to make sure that extra memory not consumed when
+            running functions that normall run with requires_grad = True
+            """
+            with torch.no_grad():
+                y_true.append(y)
+
+                output = model(X)
+                y_pred.append(predict_class(output.data))
+
+                running_loss.append(criterion(output, y).item())
+
+        accuracy = metrics.accuracy_score(y_true=y_true, y_pred=y_pred)
+        precision = metrics.precision_score(y_true=y_true, y_pred=y_pred)
+        f1score = metrics.f1_score(y_true=y_true, y_pred=y_pred)
+        recall = metrics.recall_score(y_true=y_true, y_pred=y_pred)
+        
+        loss = np.mean(running_loss)
+
+        return accuracy, precision, f1score, recall, loss
+    
+    train_accuracy, train_precision, train_f1score, train_recall, train_loss = get_stats(training_loader)
+    training_results = Result("training", epoch, train_accuracy, train_precision, train_f1score, train_recall, train_loss)
+
+    val_accuracy, val_precision, val_f1score, val_recall, val_loss = get_stats(validation_loader)
+    validation_results = Result("validation", epoch, val_accuracy, val_precision, val_f1score, val_recall, val_loss)
+
+    if(includes_testing):
+        test_accuracy, test_precision, test_f1score, test_recall, test_loss = get_stats(testing_loader)
+        testing_results = Result("testing", epoch, test_accuracy, test_precision, test_f1score, test_recall, test_loss)   
 
     return
 
